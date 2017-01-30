f8e112e62ca5792cafdbad7ee5c5d347e5d26765
Dallin Parker <dallin.r.parker@gmail.com>
2017-01-30 15:39:06 -0500

    fixed bug

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(stormpath.init(app, {
 }));
 
 app.get('/', function(req, res) {
-  res.send('Hey there! thanks for visiting my site, be sure to <a hred='/login>login</a>!');
-});
+  res.send("Hey there! thanks for visiting my site, be sure to <a href='/login'>login</a>!");
+};
 
 app.listen(process.env.PORT || 3000);
