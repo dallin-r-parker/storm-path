7c65e42a52a825421c3f77677d42e9c8a59746be
Dallin Parker <dallin.r.parker@gmail.com>
2017-01-30 16:04:40 -0500

    fixed indexjs file on line 12

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(stormpath.init(app, {
 
 app.get('/', function(req, res) {
   res.send("Hey there! thanks for visiting my site, be sure to <a href='/login'>login</a>!");
-};
+});
 
 app.listen(process.env.PORT || 3000);
